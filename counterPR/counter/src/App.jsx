d7b1680f011681fcf6a3fe2a6b83587142c6bcb3
vishal jadhav <vj7708152@gmail.com>
2024-03-29 14:29:43 +0530

    effect

--- counterPR/counter/src/App.jsx
+++ counterPR/counter/src/App.jsx
@@ -1,22 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [counter, setCounter] = useState(5);
+//componenet mounting
+  useEffect(()=>{
+   console.log("Mounted");
+  },[])
 
+//component updation
+  useEffect(()=>{
+   console.log("comp Updated ");
+  },[counter])
+
+// component unmounting
+useEffect(() => {
+  console.log("comp Updated ");
+  // Define a function to be called when component unmounts
+  const func = () => {
+    console.log("Unmounting")
+  };
+  return () => {
+    // Call the function when the component unmounts
+    func();
+  };
+}, []);
+ 
   // let counter=5;
 
   const addValue = () => {
     // console.log("value added",Math.random())
     // console.log(counter);
     // counter+=1;
-    // setCounter(counter + 1);//so  this is bunch of packages and still counter 1 only 
-    // setCounter(counter + 1);
-    // setCounter(counter + 1);
-    // setCounter(counter + 1);
-    setCounter(prevcounter =>prevcounter+1);//this will add 3 in 5 onclick result is 8
-    setCounter(prevcounter =>prevcounter+1);//we take prev state and this is not bundled state
-    setCounter(prevcounter =>prevcounter+1);
+    setCounter(counter + 1);//so  this is bunch of packages and still counter 1 only 
+    setCounter(counter + 1);
+    setCounter(counter + 1);
+    setCounter(counter + 1);
+    // setCounter(prevcounter =>prevcounter+1);//this will add 3 in 5 onclick result is 8
+    // setCounter(prevcounter =>prevcounter+1);//we take prev state and this is not bundled state
+    // setCounter(prevcounter =>prevcounter+1);
   };
 
   const removeValue = () => {
